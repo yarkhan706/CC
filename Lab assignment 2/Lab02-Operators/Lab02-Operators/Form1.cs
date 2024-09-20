6389b093e9dbfc6949536916a0a9f1512ec830b3
ASFAND YAR KHAN <139148529+yarkhan706@users.noreply.github.com>
2024-09-20 14:03:43 +0500

    Add files via upload

---
+++ Lab assignment 2/Lab02-Operators/Lab02-Operators/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Lab02_Operators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String var = richTextBox1.Text;
+            // split the input on the basis of space
+            String[] words = var.Split(' ');
+            // Regular Expression for operators
+            Regex regex1 = new Regex(@"^[+| -|*|/]$");
+            for (int i = 0; i < words.Length; i++)
+            {
+                Match match1 = regex1.Match(words[i]);
+                if (match1.Success)
+                {
+                    richTextBox2.Text += words[i] + " ";
+                }
+                else
+                {
+                    MessageBox.Show("invalid " + words[i]);
+                }
+            }
+
+        }
+    }
+}
