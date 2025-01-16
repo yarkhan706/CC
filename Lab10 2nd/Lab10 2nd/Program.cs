3353408073950a0dbc0ef3865f3e89ba68ebe23c
ASFAND YAR KHAN <139148529+yarkhan706@users.noreply.github.com>
2025-01-16 17:22:28 +0500

    Add files via upload

---
+++ Lab10 2nd/Lab10 2nd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_2nd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList States = new ArrayList();
+            Stack<String> Stack = new Stack<String>();
+            String Parser;
+            String[] Col = { "begin" ,"(",")","{","int","a","b",
+"c","=","5","10","0",";","if",">","print",
+"else","$","}","+","end","Program","DecS","AssS","IffS","PriS","Var","Const"
+};
+        }
+    }
+}
